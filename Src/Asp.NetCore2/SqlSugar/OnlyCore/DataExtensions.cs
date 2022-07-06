18090e4e7ffd53aeba5f3c73a90444481737741f
CyrusZhou <lsqtzj@msn.com>
2022-07-06 14:45:55 +0200

    Sqlite 数据库 db.Insertable(insertObj).ExecuteCommand(); 和 db.Fastest<DC_Scene>().BulkCopy(lstData); 写入的datetime数据 格式不同 https://github.com/donet5/SqlSugar/issues/1042

--- Src/Asp.NetCore2/SqlSugar/OnlyCore/DataExtensions.cs
+++ Src/Asp.NetCore2/SqlSugar/OnlyCore/DataExtensions.cs
@@ -370,11 +370,18 @@ namespace SqlSugar
                 for (int i = 0; i < dr.FieldCount; i++)
                 {
                     string name = dr.GetName(i).Trim();
+                    Type type = dr.GetFieldType(i);
+                    if (dr.GetDataTypeName(i) == "datetime")
+                    {
+                        type = typeof(DateTime);
+                    }
                     if (!columns.Contains(name))
-                        columns.Add(new DataColumn(name, dr.GetFieldType(i)));
+                    {
+                        columns.Add(new DataColumn(name, type));
+                    }
                     else
                     {
-                        columns.Add(new DataColumn(name + i, dr.GetFieldType(i)));
+                        columns.Add(new DataColumn(name + i, type));
                     }
                 }
 
@@ -411,11 +418,18 @@ namespace SqlSugar
                     for (int i = 0; i < dr.FieldCount; i++)
                     {
                         string name = dr.GetName(i).Trim();
-                        if (!columns.Contains(name))
-                            columns.Add(new DataColumn(name, dr.GetFieldType(i)));
+                        Type type = dr.GetFieldType(i);
+                        if(dr.GetDataTypeName(i) == "datetime")
+                        {
+                            type = typeof(DateTime);
+                        }
+                        if (!columns.Contains(name)) 
+                        {
+                            columns.Add(new DataColumn(name, type));
+                        }
                         else
                         {
-                            columns.Add(new DataColumn(name + i, dr.GetFieldType(i)));
+                            columns.Add(new DataColumn(name + i, type));
                         }
                     }
 
